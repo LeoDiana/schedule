a07833aa0b79cbb558c45c98c70ee6e9e208070d
dlyvytska <dianaliviz2001@gmail.com>
2022-04-25 20:36:46 +0300

    add academic-status controller

--- server/src/academic-status/academic-status/academic-status.controller.ts
+++ server/src/academic-status/academic-status/academic-status.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { AcademicStatusService } from './academic-status.service';
+import { AcademicStatus } from '../academic-status.entity';
 
 @Controller('academic-status')
-export class AcademicStatusController {}
+export class AcademicStatusController {
+  constructor(private academicStatusService: AcademicStatusService) {}
+
+  @Get()
+  async findAll(): Promise<AcademicStatus[]> {
+    return await this.academicStatusService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<AcademicStatus> {
+    return await this.academicStatusService.findOne(id);
+  }
+
+  @Post()
+  async create(@Body() teacher: AcademicStatus): Promise<AcademicStatus> {
+    return await this.academicStatusService.create(teacher);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() teacher: AcademicStatus,
+  ): Promise<AcademicStatus> {
+    return await this.academicStatusService.update(teacher);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    await this.academicStatusService.remove(id);
+  }
+}
