bf05b7465efd31576ef70eccc3cb8944d0bb18b6
Diana <dianaliviz2001@gmail.com>
2022-05-08 18:51:38 +0300

    add more info objects

--- client/src/types/entitiesInfo.js
+++ client/src/types/entitiesInfo.js
@@ -73,19 +73,141 @@ export const teacherInfo = {
 teacherInfo.CreateForm = generateCommonFormFor('create', teacherInfo);
 teacherInfo.UpdateForm = generateCommonFormFor('update', teacherInfo);
 
-// export const teacherInfo = {
-//   name: 'Name',
-//   fields: {
-//   },
-//   shortShownName: (obj) => {
-//     return `${obj.shortName}.`;
-//   },
-//   api: generateApiCallsObjectFor(ENDPOINTS.),
-//   CreateForm: null,
-//   UpdateForm: null,
-// };
-// academicStatusInfo.CreateForm = generateCommonFormFor('create', academicStatusInfo);
-// academicStatusInfo.UpdateForm = generateCommonFormFor('update', academicStatusInfo);
+export const subjectInfo = {
+  name: 'Subject',
+  fields: {
+    name: { label: 'Name', type: 'text' },
+    shortName: { label: 'Short name', type: 'text' },
+  },
+  shortShownName: (obj) => {
+    return `${obj.shortName}.`;
+  },
+  api: generateApiCallsObjectFor(ENDPOINTS.subject),
+  CreateForm: null,
+  UpdateForm: null,
+};
+subjectInfo.CreateForm = generateCommonFormFor('create', subjectInfo);
+subjectInfo.UpdateForm = generateCommonFormFor('update', subjectInfo);
+
+export const lessonTypeInfo = {
+  name: 'Lesson type',
+  fields: {
+    name: { label: 'Name', type: 'text' },
+    shortName: { label: 'Short name', type: 'text' },
+  },
+  shortShownName: (obj) => {
+    return `${obj.shortName}.`;
+  },
+  api: generateApiCallsObjectFor(ENDPOINTS.lessonType),
+  CreateForm: null,
+  UpdateForm: null,
+};
+lessonTypeInfo.CreateForm = generateCommonFormFor('create', lessonTypeInfo);
+lessonTypeInfo.UpdateForm = generateCommonFormFor('update', lessonTypeInfo);
+
+export const lessonTimeInfo = {
+  name: 'Lesson time',
+  fields: {
+    number: { label: 'Number', type: 'text' },
+    timeStart: { label: 'Time start', type: 'text' },
+    timeEnd: { label: 'Time end', type: 'text' },
+  },
+  shortShownName: (obj) => {
+    return `${obj.number} ${obj.timeStart}`;
+  },
+  api: generateApiCallsObjectFor(ENDPOINTS.lessonTime),
+  CreateForm: null,
+  UpdateForm: null,
+};
+lessonTimeInfo.CreateForm = generateCommonFormFor('create', lessonTimeInfo);
+lessonTimeInfo.UpdateForm = generateCommonFormFor('update', lessonTimeInfo);
+
+export const dayInfo = {
+  name: 'Day',
+  fields: {
+    name: { label: 'Name', type: 'text' },
+  },
+  shortShownName: (obj) => {
+    return `${obj.name}`;
+  },
+  api: generateApiCallsObjectFor(ENDPOINTS.day),
+  CreateForm: null,
+  UpdateForm: null,
+};
+dayInfo.CreateForm = generateCommonFormFor('create', dayInfo);
+dayInfo.UpdateForm = generateCommonFormFor('update', dayInfo);
+
+export const weekTypeInfo = {
+  name: 'Week type',
+  fields: {
+    name: { label: 'Name', type: 'text' },
+  },
+  shortShownName: (obj) => {
+    return `${obj.name}`;
+  },
+  api: generateApiCallsObjectFor(ENDPOINTS.weekType),
+  CreateForm: null,
+  UpdateForm: null,
+};
+weekTypeInfo.CreateForm = generateCommonFormFor('create', weekTypeInfo);
+weekTypeInfo.UpdateForm = generateCommonFormFor('update', weekTypeInfo);
+
+export const buildingInfo = {
+  name: 'Building',
+  fields: {
+    name: { label: 'Name', type: 'text' },
+    address: { label: 'Address', type: 'text' },
+  },
+  shortShownName: (obj) => {
+    return `${obj.name}`;
+  },
+  api: generateApiCallsObjectFor(ENDPOINTS.building),
+  CreateForm: null,
+  UpdateForm: null,
+};
+buildingInfo.CreateForm = generateCommonFormFor('create', buildingInfo);
+buildingInfo.UpdateForm = generateCommonFormFor('update', buildingInfo);
+
+export const classroomInfo = {
+  name: 'Classroom',
+  fields: {
+    number: { label: 'Number', type: 'text' },
+    capacity: { label: 'Capacity', type: 'number' },
+    building: {
+      label: 'Building',
+      type: 'entity',
+      getEntitiesForList: async () => {
+        return await readEntitiesApi(ENDPOINTS.building);
+      },
+      makeShortShownName: (obj) => buildingInfo.shortShownName(obj),
+    },
+  },
+  shortShownName: (obj) => {
+    return `${obj.number} ${obj.building.makeShortShownName(obj)}`;
+  },
+  api: generateApiCallsObjectFor(ENDPOINTS.classroom),
+  CreateForm: null,
+  UpdateForm: null,
+};
+classroomInfo.CreateForm = generateCommonFormFor('create', classroomInfo);
+classroomInfo.UpdateForm = generateCommonFormFor('update', classroomInfo);
+
+export const subgroupInfo = {
+  name: 'Group',
+  fields: {
+    name: { label: 'Name', type: 'text' },
+    groupName: { label: 'Group name', type: 'text' },
+    studentsNumber: { label: 'Students number', type: 'number' },
+  },
+  shortShownName: (obj) => {
+    return `${obj.groupName} ${obj.name}`;
+  },
+  api: generateApiCallsObjectFor(ENDPOINTS.subgroup),
+  CreateForm: null,
+  UpdateForm: null,
+};
+subgroupInfo.CreateForm = generateCommonFormFor('create', subgroupInfo);
+subgroupInfo.UpdateForm = generateCommonFormFor('update', subgroupInfo);
 
 const convertToKebab = (str) => {
   return str.toLowerCase().replace(' ', '-');
@@ -94,4 +216,12 @@ const convertToKebab = (str) => {
 export const commonEntitiesInfo = {
   [convertToKebab(academicStatusInfo.name)]: academicStatusInfo,
   [convertToKebab(teacherInfo.name)]: teacherInfo,
+  [convertToKebab(subjectInfo.name)]: subjectInfo,
+  [convertToKebab(lessonTypeInfo.name)]: lessonTypeInfo,
+  [convertToKebab(lessonTimeInfo.name)]: lessonTimeInfo,
+  [convertToKebab(dayInfo.name)]: dayInfo,
+  [convertToKebab(weekTypeInfo.name)]: weekTypeInfo,
+  [convertToKebab(buildingInfo.name)]: buildingInfo,
+  [convertToKebab(classroomInfo.name)]: classroomInfo,
+  [convertToKebab(subgroupInfo.name)]: subgroupInfo,
 };
