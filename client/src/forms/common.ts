4970b9511ec377a0988be8aa5b1593bf60c2ebc9
Diana <dianaliviz2001@gmail.com>
2022-07-15 15:59:31 +0300

    delete duplicates

--- client/src/forms/common.ts
+++ client/src/forms/common.ts
@@ -21,27 +21,3 @@ export const formStatusProps: FormStatusProps = {
     type: 'error',
   },
 };
-
-type FieldType = 'text' | 'number' | 'entity';
-type FormType = 'create' | 'update';
-
-export type FieldScheme<T> = {
-  // name: string;
-  label: string;
-  type: FieldType;
-  helperText?: string;
-  getEntitiesForList?(): Promise<T[]>;
-  makeShortShownName?(obj: T): string;
-};
-
-// Record<keys, Type>
-export type FieldsScheme<T> = {
-  [K in keyof T]: FieldScheme<K>; // K | Pick
-};
-
-export type FormScheme<T> = {
-  title: string;
-  apiCall(entity: T): Promise<string>;
-  fields: FieldsScheme<T>;
-  type: FormType;
-};
