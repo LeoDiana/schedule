c0f97c70869c5e7211faf2047d6ef5456c7d1657
Diana <dianalyvytska@gmail.com>
2022-09-14 12:38:31 +0300

    added endpoint for editing table

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,11 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-// @ts-nocheck
-
 import React from 'react';
 import './App.css';
 import { ScheduleFilter } from './components/ScheduleFilter';
-import ScheduleEditGrid from './components/ScheduleEditGrid';
-import Schedule from './pages/Schedule';
 
 
 function App() {
   return <>
-    {/* <ScheduleFilter /> */}
-    <ScheduleEditGrid />
+    <ScheduleFilter />
   </>;
 }
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,15 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import { store } from './app/store';
-import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import AdminPanel from "./pages/AdminPanel";
-import Schedule from "./pages/Schedule";
-// import { commonEntitiesInfo } from './common/entitiesInfo';
-// import { EntityPage } from './pages/EntityPage';
-// import { convertToKebab } from './common/utilities';
+import ScheduleEditGrid from './components/ScheduleEditGrid';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
@@ -19,15 +14,8 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App/>}/>
-        {/* {Object.keys(commonEntitiesInfo).map((entityName) => ( */}
-        {/*   <Route */}
-        {/*     key={entityName} */}
-        {/*     path={`/${convertToKebab(entityName)}`} */}
-        {/*     element={<EntityPage {...commonEntitiesInfo[entityName]} />} */}
-        {/*   /> */}
-        {/* ))} */}
         <Route path="admin-panel" element={<AdminPanel/>}/>
-        {/* <Route path="schedule" element={<Schedule/>}/> */}
+        <Route path="edit-schedule" element={<ScheduleEditGrid/>}/>
       </Routes>
     </BrowserRouter>
     {/* </Provider> */}
